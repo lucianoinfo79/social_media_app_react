714ae3fec8d487ee1770775af29084ba25a10007
unknown <lucianoinfo79@gmail.com>
2023-01-24 21:52:28 -0300

    frontend forms

---
+++ frontend/src/scenes/homePage/index.js
@@ -0,0 +1,13 @@
+import { Box } from "@mui/material";
+import NavBar from "../navbar";
+
+
+const HomePage = () => {
+    return(
+        <Box>
+            <NavBar />
+        </Box>
+    )
+}
+
+export default HomePage;
